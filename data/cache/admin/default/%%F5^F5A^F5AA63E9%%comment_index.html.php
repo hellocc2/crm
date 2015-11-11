85ce706672a80e22d3fbbf5d94038f6ca7b7b74b
jiangcai <jiangcai@milanoo.com>
2015-11-11 11:59:48 +0800

    111

--- data/cache/admin/default/%%F5^F5A^F5AA63E9%%comment_index.html.php
+++ data/cache/admin/default/%%F5^F5A^F5AA63E9%%comment_index.html.php
@@ -1,5 +1,7 @@
-<?php /* Smarty version 2.6.18, created on 2015-11-10 16:36:28
+<?php /* Smarty version 2.6.18, created on 2015-11-11 11:56:52
          compiled from comment_index.html */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('function', 'math', 'comment_index.html', 191, false),)), $this); ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => "header2.html", 'smarty_include_vars' => array()));
 $this->_tpl_vars = $_smarty_tpl_vars;
@@ -186,7 +188,23 @@ css/story.css?r=<?php echo $this->_tpl_vars['REVISION']; ?>
    	</div><!--/col-8-->
 	
 	<div class="col-md-4">
-	
+		<div class="formline">
+		<form action="?module=comment&action=add">
+		  <div class="form-group">
+			<label for="comment">泡桐树小学</label>
+			<textarea class="form-control" rows="20" id="comment"></textarea>
+		  </div>
+		   <div class="form-group">
+			<label for="yzm">输入验证码:</label>
+			<input  class="verification" name="verifycode" type="text"  maxlength=4 onFocus="this.value=''" autocomplete="off"/>
+			<a href="javascript:changeimg('comment')">
+				<img id="vfcodesuggest" src="?module=auth&action=captcha&act=comment&<?php echo smarty_function_math(array('equation' => 'rand(100,10000)'), $this);?>
+" style="border:1px solid #e4eef9;height: 30px;" align="absmiddle" alt="换一张">
+			</a>
+		  </div>
+		   <button type="submit" class="btn btn-default">发表言论</button>
+		</form>
+		</div>
 	</div><!--/col-4-->
 	
   </div>
@@ -197,4 +215,10 @@ $this->_smarty_include(array('smarty_include_tpl_file' => "footer.html", 'smarty
 $this->_tpl_vars = $_smarty_tpl_vars;
 unset($_smarty_tpl_vars);
  ?>
+<script>
+function changeimg(act)
+{
+	$('#vfcode'+act).attr('src','v2.crm.com/auth/captcha/?act='+ act + '&' + Math.random());
+}
+</script>
 </html>
