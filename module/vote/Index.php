e9ed388cfc64346ff5f2fd84fde0017a3d8f1230
jiangcai <jiangcai@milanoo.com>
2015-11-18 17:27:54 +0800

    11

--- module/vote/Index.php
+++ module/vote/Index.php
@@ -6,8 +6,9 @@ use Helper\CheckLogin as CheckLogin;
 class Index extends \Lib\common\Application {
 	public function __construct() {
 		$tpl = \Lib\common\Template::getSmarty ();
-        $db = \Lib\common\Db::get_db();
-		$action=R::getParams ('action');
+		$id=R::getParams ('id');//1大学2专业...
+		$pid=R::getParams ('pid');//学校ID,专业ID...
+		
 		
 		$tpl->assign('action',$action);
         $tpl->display ('vote_index.html');
